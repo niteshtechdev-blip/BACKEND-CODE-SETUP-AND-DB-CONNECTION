6bafbb30e79b0fe8d5306974900bccb46711d6d5
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-07-25 08:22:19 +0530

    config

--- src/configrations.js
+++ src/configrations.js
@@ -3,15 +3,19 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 // const dotenv=require('dotenv').config()// configratiion in case of require (commonjs)
 import dotenv from 'dotenv'
-const app=express()
+const WebServer=express()
 
-app.use(express.json())// for allow json in owr server
-app.use(express.urlencoded({limit:"10kb"}))// for url data encode example nitesh saini== nitesh%20saini means %20 for space
-app.use(express.static("public_assets_folder"))// for file that server find in given folder and serve directly ex- localhost:3000/img.jpg
-app.use(cors({
+WebServer.use(express.json())// for allow json in owr server
+WebServer.use(express.urlencoded({limit:"10kb"}))// for url data encode example nitesh saini== nitesh%20saini means %20 for space
+WebServer.use(express.static("public_assets_folder"))// for file that server find in given folder and serve directly ex- localhost:3000/img.jpg
+WebServer.use(cors({
     origin:process.env.CORS_ORIGIN
 }))
-app.use(cookieParser())
+WebServer.use(cookieParser())
 dotenv.config({            // dotenv configration when we import dotenv
     path:'./env'
 })
+
+
+
+export{WebServer}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import dotenv from "dotenv"
 import { connectDB } from "./db/db_connection.js"
+import { WebServer } from "./configrations.js"
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+WebServer.listen(`${process.env.PORT}`,()=>{
+    console.log("Server is ready")
+})
